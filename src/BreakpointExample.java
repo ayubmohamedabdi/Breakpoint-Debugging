02ffae1ec7d6cefa984f5388cbc119bc52e24241
ayubmohamedabdi <128827849+ayubmohamedabdi@users.noreply.github.com>
2024-08-28 20:47:14 +0300

    Fixed the error in the code

--- src/BreakpointExample.java
+++ src/BreakpointExample.java
@@ -1,12 +1,12 @@
 public class BreakpointExample {
     public static void main(String[] args) {
         int a = 10;
-        int b = 0; // This will cause an error when we try to divide by zero
+        int b = 1; // This will not cause an error when we try to divide by 1
         int result = divide(a, b);
         System.out.println("Result: " + result);
     }
 
     public static int divide(int a, int b) {
-        return a / b; // Bug here: dividing by zero
+        return a / b; // No Bug here: dividing by 1
     }
 }
